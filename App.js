5fff55ffbf9258566ec398a7ef91f39bd5c5781f
brucemug <mugzbru199@gmail.com>
2024-01-18 08:36:06 +0300

    added screens

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomIcon from './components/CustomIcon';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open it up App.js to start working on your apps! src/assets</Text>
+      <Text>Open it up App.js to start working on your apps!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+    
+  // },
 });
