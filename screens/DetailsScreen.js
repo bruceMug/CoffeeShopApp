018f55d2c85ca1469466eae9b57d26e830f60759
brucemug <mugzbru199@gmail.com>
2024-01-18 09:23:14 +0300

    created stack nav for details page

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function DetailsScreen() {
+    return (
+        <View style={styles.container}>
+        <Text>Details Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/PaymentScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function PaymentScreen() {
+    return (
+        <View style={styles.container}>
+        <Text>Payment Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
